cfdbc22a0b75ec4397b7785bebb9ab6ba7082d59
Loice Meyo <loicemeyo@Loices-MacBook-Pro.local>
2018-07-04 18:07:31 +0300

    Include comments and logical variable names

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -12,14 +12,16 @@ class BadgeTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
     def test_it_rejects_bad_signature(self):
-        r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
-        self.assertEqual(400, r.status_code)
+        response = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
+        #Assert status code for method response (rejects bad signature)
+        self.assertEqual(400, response.status_code)
 
     def test_it_returns_svg(self):
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
         sig = sig[:8].decode("utf-8")
         url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
 
-        r = self.client.get(url)
-        self.assertEqual(200, r.status_code)
+        response = self.client.get(url)
+        #Assert that it returns svg
+        self.assertEqual(200, response.status_code)
     
