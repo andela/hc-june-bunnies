e64551f70ad197a1c83620e308993856e049b361
dmaish <mainadaniel81@gmail.com>
2018-07-05 18:13:36 +0300

    edit one letter variables to full names variables

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -7,33 +7,31 @@ class PauseTestCase(BaseTestCase):
     def test_it_works(self):
         check = Check(user=self.alice, status="up")
         check.save()
-
         url = "/api/v1/checks/%s/pause" % check.code
-        r = self.client.post(url, "", content_type="application/json",
+        response = self.client.post(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
 
-        result = r.json()
+        result = response.json()
         self.assertEqual(result["status"], "paused")
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         
-
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
         check.save()
     
         url = "/api/v1/checks/%s/pause" % check.code
-        r = self.client.post(url,"", content_type="application/json",
+        response = self.client.post(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
         
-        self.assertEqual(r.status_code, 400)
+        self.assertEqual(response.status_code, 400)
 
     def test_only_allows_post_requests(self):
         check = Check(user=self.bob, status="up")
         check.save()
 
         url = "/api/v1/checks/%s/pause" % check.code
-        r = self.client.get(url, "", content_type="application/json",
+        response = self.client.get(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
-        self.assertEqual(r.status_code, 405)
+        self.assertEqual(response.status_code, 405)
 
         
