ecd5e3a8084c7952dd1350b67518003adcf0a9a6
dmaish <mainadaniel81@gmail.com>
2018-07-12 14:45:21 +0300

    send notification email if the check is pinged too often

--- hc/api/models.py
+++ hc/api/models.py
@@ -70,7 +70,7 @@ class Check(models.Model):
         return "%s@%s" % (self.code, settings.PING_EMAIL_DOMAIN)
 
     def send_alert(self):
-        if self.status not in ("up", "down"):
+        if self.status not in ("up", "down", "often"):
             raise NotImplementedError("Unexpected status: %s" % self.status)
 
         errors = []
