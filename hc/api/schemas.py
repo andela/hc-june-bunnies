be6b08d8b109ad924cb0e86efed03be61308081b
Kyalo M <jmutuku95@gmail.com>
2018-07-16 10:49:25 +0300

    Update check schema

--- hc/api/schemas.py
+++ hc/api/schemas.py
@@ -2,6 +2,8 @@ check = {
     "properties": {
         "name": {"type": "string"},
         "tags": {"type": "string"},
+        "critical": {"type": "boolean"},
+        "department": {"type": "string", "maximum": 30},
         "timeout": {"type": "number", "minimum": 60, "maximum": 15552000},
         "grace": {"type": "number", "minimum": 60, "maximum": 15552000},
         "channels": {"type": "string"}
