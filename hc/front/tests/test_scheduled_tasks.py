6e22f109e995f20239954f627dc003093bcc6121
miritih <ericmwenda5@gmail.com>
2018-07-20 12:55:37 +0300

    Add front tests

---
+++ hc/front/tests/test_scheduled_tasks.py
@@ -0,0 +1,23 @@
+from hc.api.models import Task
+from hc.test import BaseTestCase
+from datetime import timedelta as td
+from django.utils import timezone
+
+
+class ScheduledTaskTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(ScheduledTaskTestCase, self).setUp()
+        self.task = Task(task_scheduler=self.alice, subject="Alice task Here")
+        self.task.schedule = "* * * * *"
+        self.task.save()
+
+    def test_it_works(self):
+        self.client.login(username="alice@example.com", password="password")
+        
+        for email in ("alice@example.org", "bob@example.org"):
+            self.client.login(username=email, password="password")
+            response = self.client.get("/tasks/")
+            self.assertEqual(response.status_code, 200)
+
+    
